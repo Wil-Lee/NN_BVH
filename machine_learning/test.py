a00eff4f6e8774288cfc5e47b580750883f741aa
Michael Mildt <ufwfz@student.kit.edu>
2024-07-06 18:33:44 +0200

    adding time benches for testclass

--- machine_learning/test.py
+++ machine_learning/test.py
@@ -1,40 +1,48 @@
+from contextlib import contextmanager
 from nn_parser import *
 from nn_AABB import *
 from nn_data_augmentation import *
 from nn_types import *
 import pyvista as pv
+import time
 
+@contextmanager
+def bench(name):
+    print(f"\nStart {name}...")
+    start_time = time.time()
+    yield
+    end_time = time.time()
+    print(f"... {name} done in: " + str(end_time - start_time) + " seconds.")
 
 
 path = "machine_learning/conference.obj"
 
-primitives = parse_obj_file_with_primitives(path)
-aabb = get_AABB_from_primitives(primitives)
-loader = DataLoaderFromPrimitives(1000000, 1, primitives, aabb)
-
-p_mesh = parse_obj_file_with_meshes(path)
+#primitives = parse_obj_file_with_primitives(path)
+#aabb = get_AABB_from_primitives(primitives)
+#loader = DataLoaderFromPrimitives(1000000, 1, primitives, aabb)
+with bench('Parsing'):
+    p_mesh = parse_obj_file_with_meshes(path)
 primitives = p_mesh.primitives
-aabb = get_AABB_from_primitives(primitives)
-loader = DataLoaderFromMeshes(1000000, 1, p_mesh, aabb)
+with bench('Building AABB'):
+    aabb = get_AABB_from_primitives(primitives)
+batch_size = 1
+loader = DataLoaderFromMeshes(1000000, batch_size, p_mesh, aabb)
 
-#loader.__getitem__(0)
-#test = 1
-
-
-plotter = pv.Plotter()
-points = np.array([vertex for primitive in primitives for vertex in primitive])
-n_faces = len(primitives)
-faces = np.hstack([[3, 3*i, 3*i+1, 3*i+2] for i in range(n_faces)])
-mesh = pv.PolyData(points, faces)
-plotter.add_mesh(mesh, color='lightblue', show_edges=True)
-plotter.show()
-plotter.clear()
-
-
-
-for i in range(0,100):
+if 0:
     plotter = pv.Plotter()
-    primitives = loader.__getitem__(0)[0]
+    points = np.array([vertex for primitive in primitives for vertex in primitive])
+    n_faces = len(primitives)
+    faces = np.hstack([[3, 3*i, 3*i+1, 3*i+2] for i in range(n_faces)])
+    mesh = pv.PolyData(points, faces)
+    plotter.add_mesh(mesh, color='lightblue', show_edges=True)
+    plotter.show()
+    plotter.clear()
+
+
+for i in range(0,1):
+    plotter = pv.Plotter()
+    with bench(f'Generating batch (size: {batch_size}) of transformed scenes'):
+        primitives = loader.__getitem__(0)[0]
 
     points = np.array([vertex for primitive in primitives for vertex in primitive])
 
@@ -47,9 +55,3 @@ for i in range(0,100):
 
     plotter.show()
     plotter.clear()
-
-
-
-###morgen einen transformer schreiben, der die Szene in ein gescheites boundary parst. Nach dem einlesen von Parser().
-#und noch ein paar prints einfügen und zeitmessen wie lange die einzelnen Abschnitte brauchen.
-#morgen mal den stand pushen!
