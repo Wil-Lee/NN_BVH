096c138ca7cce7ba220afa38ce91249f9d730df7
Michael Mildt <ufwfz@student.kit.edu>
2024-07-06 17:38:30 +0200

    add parser for obj files

---
+++ machine_learning/nn_parser.py
@@ -0,0 +1,72 @@
+from nn_mesh_list import *
+from nn_types import *
+    
+
+def parse_obj_file_with_primitives(file_path: str):
+    """ Parses the provided file into a set of primitives. """
+    vertices: list[Vertex3] = []
+    primitives: list[Primitive3] = []
+    with open(file_path, 'r') as obj_file:
+
+        for l in obj_file:
+            # remove strip if parsing takes too long
+            line = l.strip().split()
+            if not len(line):
+                continue
+
+            if line[0] == 'v':
+                vertices.append((float(line[1]), float(line[2]), float(line[3])))
+                continue 
+
+            if line[0] == 'f':
+                vertex_indices = [] # for the current face
+                for i in range(1, len(line)):
+                    vertex_indices.append(int(line[i].split('/')[0]) - 1)
+
+                primitives.append((vertices[vertex_indices[0]], 
+                                    vertices[vertex_indices[1]], 
+                                    vertices[vertex_indices[2]]))
+    return primitives
+
+
+def parse_obj_file_with_meshes(file_path: str):
+    """ Parses the provided file into a set of meshes. """
+    mesh_list = Mesh3List()
+    
+    mesh: Mesh3 = []
+    mesh_idx = -1
+    vertices: list[Vertex3] = []
+
+    with open(file_path, 'r') as obj_file:
+
+        for l in obj_file:
+            # remove strip if parsing takes too long
+            line = l.strip().split()
+            if not len(line):
+                continue
+
+            if line[0] == 'v':
+                vertices.append((float(line[1]), float(line[2]), float(line[3])))
+                continue 
+
+            if line[0] == 'f':
+                vertex_indices = [] # for the current face
+                for i in range(1, len(line)):
+                    vertex_indices.append(int(line[i].split('/')[0]) - 1)
+
+                mesh.append(((vertices[vertex_indices[0]], 
+                              vertices[vertex_indices[1]], 
+                              vertices[vertex_indices[2]])))
+                continue
+
+            if line[0] == 'o':
+                mesh_list.append(mesh)
+                mesh_idx += 1
+                mesh = []
+    
+    # last mesh was not added during loop
+    mesh_list.append(mesh)
+    # correct unwanted side effect caused by loop
+    del mesh_list.mesh_indices[0]
+
+    return mesh_list
