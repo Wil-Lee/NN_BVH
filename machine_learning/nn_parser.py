6278e7391fe3a2d842ca3c5e7c3afbbed034772c
Michael Mildt <ufwfz@student.kit.edu>
2024-07-08 13:35:41 +0200

    add scene scaling

--- machine_learning/nn_parser.py
+++ machine_learning/nn_parser.py
@@ -75,3 +75,53 @@ def parse_obj_file_with_meshes(file_path: str):
     del mesh_list.mesh_indices[0]
 
     return mesh_list
+
+
+def scale_scene(primitives: list[Primitive3], lower_limit: int = 0, upper_limit: int = 1):
+    """ Scales the largest dimension of the scene down to [lower_limit, upper_limit] and all other dimension accordingly. """
+
+    bounds = get_AABB_from_primitives(primitives)
+    
+    # shift scene into positive space
+    if bounds.x_min < 0:
+        x_shift = abs(bounds.x_min)
+        for primitive3 in primitives:
+            primitive3[0][0] += x_shift
+            primitive3[1][0] += x_shift
+            primitive3[2][0] += x_shift
+    if bounds.y_min < 0:
+        y_shift = abs(bounds.y_min)
+        for primitive3 in primitives:
+            primitive3[0][1] += y_shift
+            primitive3[1][1] += y_shift
+            primitive3[2][1] += y_shift
+    if bounds.z_min < 0:
+        z_shift = abs(bounds.z_min)
+        for primitive3 in primitives:
+            primitive3[0][2] += z_shift
+            primitive3[1][2] += z_shift
+            primitive3[2][2] += z_shift
+
+    # scale scene
+    max_extent = bounds.x_max - bounds.x_min
+    y_extent = bounds.y_max - bounds.y_min
+    z_extent = bounds.z_max - bounds.z_min
+    if y_extent > max_extent:
+        max_extent = y_extent
+    if z_extent > max_extent:
+        max_extent = z_extent
+    
+    for p in primitives:
+        p[0][0] /= max_extent
+        p[0][1] /= max_extent
+        p[0][2] /= max_extent
+        p[1][0] /= max_extent
+        p[1][1] /= max_extent
+        p[1][2] /= max_extent
+        p[2][0] /= max_extent
+        p[2][1] /= max_extent
+        p[2][2] /= max_extent
+
+
+    return primitives
+    
