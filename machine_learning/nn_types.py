0d3124bddb6ab3980b5cf658a95387f49f0de84a
Michael Mildt <ufwfz@student.kit.edu>
2024-07-13 19:27:20 +0200

    add type descriptions and and axis enum

--- machine_learning/nn_types.py
+++ machine_learning/nn_types.py
@@ -1,6 +1,18 @@
 from typing import Tuple
+from enum import Enum
 import numpy as np
 
+
 Vertex3 = np.ndarray
+""" 3D Vertex """
+
 Primitive3 = np.ndarray
+""" 3D Triangle Primitive"""
+
 Mesh3 = list[Primitive3]
+""" 3D Triangle Mesh """
+
+class Axis(Enum):
+    x = 0
+    y = 1
+    z = 2
