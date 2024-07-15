58aebff21c8d117a2a94fca059ec5e5bd57edf91
Michael Mildt <ufwfz@student.kit.edu>
2024-07-15 21:38:25 +0200

    add code for testing BVH implementation

--- machine_learning/test.py
+++ machine_learning/test.py
@@ -3,6 +3,7 @@ from nn_parser import *
 from nn_AABB import *
 from nn_data_augmentation import *
 from nn_types import *
+from nn_BVH import * 
 import pyvista as pv
 import time
 
@@ -14,7 +15,7 @@ def bench(name):
     end_time = time.time()
     print(f"... {name} done in: " + str(end_time - start_time) + " seconds.")
 
-def draw_scene_bounding_box(aabb: AABB, plotter):
+def draw_bounding_box(aabb: AABB, plotter, color: str='red', line_width: float=6):
     points = np.array([
         [aabb.x_min, aabb.y_min, aabb.z_min],
         [aabb.x_min, aabb.y_min, aabb.z_max],
@@ -41,32 +42,48 @@ def draw_scene_bounding_box(aabb: AABB, plotter):
     poly_data.points = points
     poly_data.lines = np.array(line_data)
 
-    plotter.add_mesh(poly_data, color='red', line_width=6)
+    plotter.add_mesh(poly_data, color=color, line_width=line_width)
     
+
+
+
+
+
 
 
 #path = "machine_learning/conference.obj"
 #path = "machine_learning/Room.obj"
-path = "machine_learning/bbedroom.pbrt"
+#path = "machine_learning/bedroomNoFloor.obj"
+
+
+
+if 1:
+    path = "machine_learning/bedroom_LowPoly.obj"
+    with bench('Parsing'):
+        p_mesh = parse_obj_file_with_meshes(path)
+    # remove floor from moveable meshes
+    p_mesh.mesh_indices.pop(0)
+else:
+    with bench('Parsing'):
+        path = "machine_learning/bbedroom.pbrt"
+        p_mesh = parse_pbrt_file_with_meshes(path)
 
 #primitives = parse_obj_file_with_primitives(path)
 #aabb = get_AABB_from_primitives(primitives)
 #loader = DataLoaderFromPrimitives(1000000, 1, primitives, aabb)
-with bench('Parsing'):
-    #p_mesh = parse_obj_file_with_meshes(path)
-    p_mesh = parse_pbrt_file_with_meshes(path)
+
 with bench('Scaling'):
     p_mesh.primitives = scale_scene(p_mesh.primitives)
 primitives = p_mesh.primitives
 with bench('Building AABB'):
-    aabb = get_AABB_from_primitives(primitives)
+    ab = get_AABB_from_primitives(primitives)
 batch_size = 1
 data_size = 1000000
-loader = DataLoaderFromMeshes(data_size, batch_size, p_mesh, aabb)
+loader = DataLoaderFromMeshes(data_size, batch_size, p_mesh, ab)
 
 if 1:
     plotter = pv.Plotter()
-    draw_scene_bounding_box(aabb, plotter)
+    draw_bounding_box(ab, plotter)
     points = np.array([vertex for primitive in primitives for vertex in primitive])
     n_faces = len(primitives)
     faces = np.hstack([[3, 3*i, 3*i+1, 3*i+2] for i in range(n_faces)])
@@ -75,21 +92,113 @@ if 1:
     plotter.show()
     plotter.clear()
 
+if 1:
+    for i in range(0,10):
+        plotter = pv.Plotter()
+        with bench(f'Generating batch (size: {batch_size}) of transformed scenes'):
+            for i in range(1):
+                primitives = loader.__getitem__(0)[0]
 
-for i in range(0,10):
-    plotter = pv.Plotter()
-    with bench(f'Generating batch (size: {batch_size}) of transformed scenes'):
-        for i in range(30):
-            primitives = loader.__getitem__(0)[0]
+        points = np.array([vertex for primitive in primitives for vertex in primitive])
+        draw_bounding_box(ab, plotter)
+        n_faces = len(primitives)
+        faces = np.hstack([[3, 3*i, 3*i+1, 3*i+2] for i in range(n_faces)])
+        
+        mesh = pv.PolyData(points, faces)
 
-    points = np.array([vertex for primitive in primitives for vertex in primitive])
-    draw_scene_bounding_box(aabb, plotter)
-    n_faces = len(primitives)
-    faces = np.hstack([[3, 3*i, 3*i+1, 3*i+2] for i in range(n_faces)])
+        plotter.add_mesh(mesh, color='lightblue', show_edges=True)
+
+        plotter.show()
+        plotter.clear()
+
+head_node = BVHNode(ab, primitives)
+
+plotter = pv.Plotter()
+points = np.array([vertex for primitive in primitives for vertex in primitive])
+n_faces = len(primitives)
+faces = np.hstack([[3, 3*i, 3*i+1, 3*i+2] for i in range(n_faces)])
+mesh = pv.PolyData(points, faces)
+
+plotter.add_mesh(mesh, color='lightblue', show_edges=True)
+nodes_to_split: list[BVHNode] = []
+nodes_to_split.append(head_node)
+
+level = 0
+while len(nodes_to_split) != 0:
     
-    mesh = pv.PolyData(points, faces)
-
+    new_nodes = []
+    aabbs_to_draw = []
+    
+    plotter = pv.Plotter()
     plotter.add_mesh(mesh, color='lightblue', show_edges=True)
 
+    print(f"\nStart Splitting nodes on level {level}...")
+    total_time = 0
+    for i, n in enumerate(nodes_to_split):
+
+        if n.is_leaf:
+            continue
+
+        max_x_extent = (n.aabb.x_max - n.aabb.x_min)
+        max_y_extent = (n.aabb.y_max - n.aabb.y_min)
+        max_z_extent = (n.aabb.z_max - n.aabb.z_min)
+                        
+        # split the axis with the biggest extend in half
+        if max_x_extent >= max_y_extent and max_x_extent >= max_z_extent:
+            split_axis_pos = n.aabb.x_min + (max_x_extent / 2)
+            start_time = time.time()
+            n.split(Axis.x, split_axis_pos)
+            end_time = time.time()
+        elif max_y_extent >= max_z_extent:
+            split_axis_pos = n.aabb.y_min + (max_y_extent / 2)
+            start_time = time.time()
+            n.split(Axis.y, split_axis_pos)
+            end_time = time.time()
+        else:
+            split_axis_pos = n.aabb.z_min + (max_z_extent / 2) 
+            start_time = time.time()
+            n.split(Axis.z, split_axis_pos)
+            end_time = time.time()
+        
+        total_time += end_time - start_time
+        
+        new_nodes.append(n.left_child)
+        new_nodes.append(n.right_child)
+
+        random_color = [np.random.uniform(0,1), np.random.uniform(0,1), np.random.uniform(0,1)]
+        aabbs_to_draw.append((n.aabb, np.copy(random_color)))
+
+    print(f"... Splitting nodes on level {level} done in: " + str(total_time) + " seconds.")
+
+    for ab in reversed(aabbs_to_draw):
+        draw_bounding_box(ab[0], plotter, color=ab[1])
+    
     plotter.show()
     plotter.clear()
+        
+    nodes_to_split = new_nodes
+    level += 1
+           
+      
+
+plotter.close()
+    
+    
+
+
+plotter.clear()
+
+i = 5
+
+
+
+
+
+"""
+TODO:
+- testen, ob die BVH node implementierung passt
+- EPO implementieren
+- left / right child Nodes in BVH Node einfügen
+-
+"""
+
