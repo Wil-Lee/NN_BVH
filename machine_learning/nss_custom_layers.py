38da1a5087631d0a9dbbac0295e393e4180977bd
Michael Mildt <ufwfz@student.kit.edu>
2024-08-12 21:01:12 +0200

    add scale function for primitives

--- machine_learning/nss_custom_layers.py
+++ machine_learning/nss_custom_layers.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import nss_tree_common
 from tensorflow import keras
-from tensorflow.keras.layers  import Conv2D, Flatten
+from tensorflow.keras.layers import Conv2D, Flatten
 
 class recursive_tree_level_encoder(tf.keras.layers.Layer) :
     def __init__(self, lvl, **pConfig) :
@@ -59,7 +59,7 @@ class recursive_tree_level_encoder(tf.keras.layers.Layer) :
         features = tf.einsum('bijf, bik -> bijf', features, mask)
         return (features, tf.squeeze(xyz_min), tf.squeeze(xyz_max))
 
-    @tf.function
+    #@tf.function
     def call(self, input) :
         point_cloud, node_bounds = input
 
@@ -128,25 +128,37 @@ class recursive_tree_level_encoder_EPO(tf.keras.layers.Layer) :
             use_bias=use_bias,
             name=layer_name)
     
-    @tf.function
+    #@tf.function
     def map_from_to_opt(self, points, old_min, old_max, new_min, new_max) :
         """ Scales points to [0,1) """
         do = old_max - old_min
         dn = new_max - new_min
         a = tf.math.divide_no_nan(dn, do) # scaling factor
-        # translates the points back to origin, scales them by a and translates them to new_min
+        a = tf.tile(a, [1,1,3,1])
+        old_min = tf.tile(old_min, [1,1,3,1])
+        new_min = tf.tile(new_min, [1,1,3,1])
         return tf.einsum('bpjk, bijk -> bpjk', (points - old_min), a) + new_min
 
-    @tf.function
-    def object_normalize(self, pc, mask) : # pc = point_cloud
-        # TODO: adapt min and max to work with nodes instead of coords
-        xyz_axis = tf.expand_dims(pc, axis=-1) # adds one dimension to point cloud vec (pc)
-        xyz_min = tf.reduce_min(tf.abs(xyz_axis - self.beta), axis=1, keepdims=True) + self.beta
-        xyz_max = tf.reduce_max(xyz_axis, axis=1, keepdims=True)
+    #@tf.function
+    def object_normalize(self, pc, mask):
+        
+        xyz_axis = tf.expand_dims(pc, axis=-1)
+
+        x_coords = tf.gather(xyz_axis, indices=[0, 3, 6], axis=2)
+        y_coords = tf.gather(xyz_axis, indices=[1, 4, 7], axis=2)
+        z_coords = tf.gather(xyz_axis, indices=[2, 5, 8], axis=2)
+        min_x = tf.reduce_min(tf.reduce_min(tf.abs(x_coords - self.beta), axis=1), axis=1, keepdims=True) + self.beta
+        min_y = tf.reduce_min(tf.reduce_min(tf.abs(y_coords - self.beta), axis=1), axis=1, keepdims=True) + self.beta
+        min_z = tf.reduce_min(tf.reduce_min(tf.abs(z_coords - self.beta), axis=1), axis=1, keepdims=True) + self.beta
+        max_x = tf.reduce_max(tf.reduce_max(x_coords, axis=2), axis=1, keepdims=True)
+        max_y = tf.reduce_max(tf.reduce_max(y_coords, axis=2), axis=1, keepdims=True)
+        max_z = tf.reduce_max(tf.reduce_max(z_coords, axis=2), axis=1, keepdims=True)
+
+        xyz_min = tf.expand_dims(tf.concat([min_x, min_y, min_z], axis=2), axis=-1)
+        xyz_max = tf.expand_dims(tf.concat([max_x, max_y, max_z], axis=2), axis=-1)
+
         xyz_min = tf.math.minimum(xyz_min, xyz_max)
 
-        # TODO: same for this function
-        # scales points (inside bounds) to [0,1), scales them by gamma (=1) and translates them by +(1,1,1)
         features = self.map_from_to_opt(xyz_axis,
             xyz_min, xyz_max,
             tf.zeros_like(xyz_min), tf.ones_like(xyz_min)) * self.gamma + 1
@@ -186,6 +198,11 @@ class recursive_tree_level_encoder_EPO(tf.keras.layers.Layer) :
         local_thetas = self.offset_layer2(local_thetas)
         local_thetas = self.offset_layer3(local_thetas)
         local_thetas = self.flatten(local_thetas)
+
+        x_split = tf.reduce_sum(tf.gather(local_thetas, indices=[0, 3, 6], axis=1), axis=1, keepdims=True) / 3.0
+        y_split = tf.reduce_sum(tf.gather(local_thetas, indices=[1, 4, 7], axis=1), axis=1, keepdims=True) / 3.0
+        z_split = tf.reduce_sum(tf.gather(local_thetas, indices=[2, 5, 8], axis=1), axis=1, keepdims=True) / 3.0
+        local_thetas = tf.concat([x_split, y_split, z_split], axis=1) 
 
         s = tf.stop_gradient(tf.math.divide_no_nan(max_values - min_values, node_bmax - node_bmin))
         t = tf.stop_gradient(tf.math.divide_no_nan(min_values - node_bmin, node_bmax - node_bmin))
