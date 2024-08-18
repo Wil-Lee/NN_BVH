4e26742bf881e00444bb6bfe2a6711cc51d1a5dc
Michael Mildt <ufwfz@student.kit.edu>
2024-08-18 20:32:55 +0200

    add bench file

---
+++ machine_learning/nn_tree_bench.py
@@ -0,0 +1,38 @@
+import nn_AABB
+import nn_BVH
+import nn_loss
+import nn_parser
+import nss_global_config
+
+from contextlib import contextmanager
+import time
+
+@contextmanager
+def bench(name):
+    print(f"\nStart {name}...")
+    start_time = time.time()
+    yield
+    end_time = time.time()
+    print(f"... {name} done in: " + str(end_time - start_time) + " seconds.")
+
+if __name__ == "__main__":
+    path = "machine_learning/bedroom_LowPoly.obj"
+    p_mesh = nn_parser.parse_obj_file_with_meshes(path)
+    # remove floor from moveable meshes
+    p_mesh.mesh_indices.pop(0)
+    p_mesh.primitives = nn_parser.scale_scene(p_mesh.primitives)
+    aabb = nn_AABB.get_AABB_from_primitives(p_mesh.primitives)
+    root_node = nn_BVH.BVHNode(aabb, p_mesh.primitives)
+
+    alpha = nss_global_config.EPO_SAH_alpha
+    levels = nss_global_config.lvls
+
+    with bench("Building tree"):
+        nn_BVH.build_greedy_SAH_EPO_tree_multi_thread(root_node, alpha, levels, use_epo=True)
+
+    sah_tree: float = nn_loss.SAH(root_node)
+
+    epo_tree: float = nn_loss.EPO(root_node)
+
+    print(sah_tree)
+    print(epo_tree)
