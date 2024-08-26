fix wrong mask calculation

machine_learning/nss_custom_layers.py
@@ -170,12 +170,10 @@ class recursive_tree_level_encoder_EPO(tf.keras.layers.Layer) :
 
     @tf.function
     def call(self, input) :
-        point_cloud, node_bounds = input
+        point_cloud, node_bounds, node_mask = input
 
         node_bmin = node_bounds[:, 0:3]
         node_bmax = node_bounds[:, 3:6] 
-
-        node_mask = tf.stop_gradient(nss_tree_common.build_mask_EPO(point_cloud, node_bounds))
 
         # This code from here on implements Fig 1 of the paper
         # sets all points which are not inside the current bounding volume to 0

machine_learning/nss_tree_common.py
@@ -1,5 +1,6 @@
 # credits: https://github.com/cgaueb/nss
 import tensorflow as tf
+import nss_tree_common
 
 class neural_spatial_node() :
     def __init__(self, name, lvl) :
@@ -7,11 +8,14 @@ class neural_spatial_node() :
         self.lvl = lvl
 
         self.bounds = None
-
+        self.mask = None
+        
         self.theta = None
         self.parent_bounds = None
         self.parent_offset = None
         self.parent_normal = None
+        self.parent_mask = None
+       
 
 @tf.function
 def build_mask_(point_cloud, bmin, bmax, lower_bound = 0.0, upper_bound = 1.0) :
@@ -48,54 +52,25 @@ def build_mask(point_cloud, bounds, lower_bound = 0.0, upper_bound = 1.0) :
     return build_mask_(point_cloud, bounds[:, 0:3], bounds[:, 3:], lower_bound, upper_bound)
 
 ####################################################### EDIT ##################################################################################
+import nss_tree_modules
+@tf.function
+def build_mask_EPO_(primitive_cloud, bmin, bmax, parent_offset, parent_normal, parent_mask, is_right_child, lower_bound = 0.0, upper_bound = 1.0):
+    axis_points = nss_tree_modules.get_axis_points(primitive_cloud, parent_normal)
+
+    mins = tf.reduce_min(axis_points, axis=2, keepdims=True)
+    maxs = tf.reduce_max(axis_points, axis=2, keepdims=True)
+    mids = mins + ((maxs - mins) * 0.5)
+
+    if is_right_child:
+        result = tf.einsum('bij, bij -> bij', parent_mask, tf.cast(parent_offset[..., tf.newaxis] < mids, tf.float32))
+    else:
+        result = tf.einsum('bij, bij -> bij', parent_mask, tf.cast(parent_offset[..., tf.newaxis] >= mids, tf.float32))
+
+    return result
 
 @tf.function
-def build_mask_EPO_(point_cloud, bmin, bmax, lower_bound = 0.0, upper_bound = 1.0):
-    x1, x2, x3 = tf.unstack(point_cloud[..., :3], axis=-1)
-    y1, y2, y3 = tf.unstack(point_cloud[..., 3:6], axis=-1) 
-    z1, z2, z3 = tf.unstack(point_cloud[..., 6:9], axis=-1)
-
-    x_bmin = bmin[..., 0:1]
-    y_bmin = bmin[..., 1:2]
-    z_bmin = bmin[..., 2:3]
-
-    x_bmax = bmax[..., 0:1]
-    y_bmax = bmax[..., 1:2]
-    z_bmax = bmax[..., 2:3]
-
-    mask_x = tf.where(tf.reduce_all(
-        tf.stack([
-            x1 >= x_bmin, x1 <= x_bmax,
-            x2 >= x_bmin, x2 <= x_bmax,
-            x3 >= x_bmin, x3 <= x_bmax
-        ], axis=-1), axis=-1
-    ), upper_bound, lower_bound)
-
-    mask_y = tf.where(tf.reduce_all(
-        tf.stack([
-            y1 >= y_bmin, y1 <= y_bmax,
-            y2 >= y_bmin, y2 <= y_bmax,
-            y3 >= y_bmin, y3 <= y_bmax
-        ], axis=-1), axis=-1
-    ), upper_bound, lower_bound)
-
-    mask_z = tf.where(tf.reduce_all(
-        tf.stack([
-            z1 >= z_bmin, z1 <= z_bmax,
-            z2 >= z_bmin, z2 <= z_bmax,
-            z3 >= z_bmin, z3 <= z_bmax
-        ], axis=-1), axis=-1
-    ), upper_bound, lower_bound)
-
-    mask_x = tf.expand_dims(mask_x, axis=-1)
-    mask_y = tf.expand_dims(mask_y, axis=-1)  
-    mask_z = tf.expand_dims(mask_z, axis=-1)
-
-    return tf.math.multiply(tf.math.multiply(mask_x, mask_y), mask_z)
-
-@tf.function
-def build_mask_EPO(point_cloud, bounds, lower_bound = 0.0, upper_bound = 1.0):
-    return build_mask_EPO_(point_cloud, bounds[:, 0:3], bounds[:, 3:], lower_bound, upper_bound)
+def build_mask_EPO(point_cloud, bounds, parent_offset, parent_normal, parent_mask, is_right_child, lower_bound = 0.0, upper_bound = 1.0):
+    return build_mask_EPO_(point_cloud, bounds[:, 0:3], bounds[:, 3:], parent_offset[2], parent_normal, parent_mask, is_right_child, lower_bound, upper_bound)
 
 ###############################################################################################################################################
 @tf.function